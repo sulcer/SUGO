7f63c00d64e16e35daca05bcf4a5732f1b6903af
Davorin Drozg <davorin.drozg@student.um.si>
2023-05-03 00:35:27 +0200

    feat: add mobile nav menu

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { FC, PropsWithChildren } from 'react';
+import { FC, PropsWithChildren, useState } from 'react';
 import { classNames } from '@/utils/classNames';
 import styles from './Header.module.scss';
 import Logo from '@/assets/logo-sugo.png';
+import { useRouter } from 'next/router';
+import { useLockBodyScroll } from '@/utils/hooks';
+import { MdChevronRight, MdClose, MdMenu } from 'react-icons/md';
 
 const routes = ['Strojni park', 'Izdelki', 'Kontakt'];
 
@@ -18,28 +21,90 @@ const NavigationLink: FC<PropsWithChildren & { link?: string }> = ({
   );
 };
 
-const Header = () => {
+const MobileNavMenu: FC<{ closeSelf: () => void }> = ({ closeSelf }) => {
+  useLockBodyScroll();
   return (
-    <div className={'flex flex-row sm:mx-20 my-5 px-4 sm:px-0'}>
-      <Link className={'flex flex-row grow'} href={'/'}>
-        <Image src={Logo} alt={'Logo'} width={100} height={30} />
-      </Link>
-      <div
-        className={classNames(
-          'flex flex-row items-center gap-3 sm:gap-8',
-          styles.NavLinks
-        )}
-      >
-        {routes.map((route, index) => (
-          <NavigationLink
-            key={index}
-            link={route.toLowerCase().split(' ').join('-')}
+    <div
+      className={'w-screen h-screen fixed top-0 left-0 bg-white z-10 pb-6 p-4'}
+    >
+      <div className={'container h-full flex flex-col justify-start gap-8'}>
+        <div className={'flex flex-row justify-between py-2 items-center'}>
+          <Link className={'flex flex-row grow'} href={'/'}>
+            <Image src={Logo} alt={'Logo'} width={80} height={25} />
+          </Link>
+          <button onClick={closeSelf} className={'py-1 px-1'}>
+            <MdClose className={'h-8 w-auto'} />
+          </button>
+        </div>
+        <hr className={'border-gray-300 border-1 mx-5'} />
+        <div className={'flex flex-col justify-start w-9/12 mx-auto flex-1 '}>
+          <ul
+            className={
+              'flex flex-col list-none gap-10 items-baseline text-xl mb-10'
+            }
           >
-            {route}
-          </NavigationLink>
-        ))}
+            {routes.map((navLink: string, index: number) => (
+              <li key={index} className={'block w-full'} onClick={closeSelf}>
+                <NavigationLink
+                  link={navLink.toLowerCase().split(' ').join('-')}
+                >
+                  <div
+                    className={
+                      'hover:text-gray-900 py-6 flex flex-row justify-between items-center'
+                    }
+                  >
+                    <span>{navLink}</span>
+                    <MdChevronRight className={'h-8 w-auto'} />
+                  </div>
+                </NavigationLink>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
+  );
+};
+
+const Header = () => {
+  const router = useRouter();
+  const { locale, locales } = router;
+  const [mobileMenuIsShown, setMobileMenuIsShown] = useState(false);
+  return (
+    <>
+      {!mobileMenuIsShown ? (
+        <div className={'flex flex-row sm:mx-20 my-5 px-4 sm:px-0'}>
+          <Link className={'flex flex-row grow'} href={'/'}>
+            <Image src={Logo} alt={'Logo'} width={100} height={30} />
+          </Link>
+          <div
+            className={classNames(
+              'flex flex-row items-center gap-3 sm:gap-8 hidden sm:flex',
+              styles.NavLinks
+            )}
+          >
+            {routes.map((route, index) => (
+              <NavigationLink
+                key={index}
+                link={route.toLowerCase().split(' ').join('-')}
+              >
+                {route}
+              </NavigationLink>
+            ))}
+          </div>
+          <div className={'flex'}>
+            <button
+              onClick={() => setMobileMenuIsShown(true)}
+              className={'p-1 block sm:hidden'}
+            >
+              <MdMenu className={'h-8 w-auto'} />
+            </button>
+          </div>
+        </div>
+      ) : (
+        <MobileNavMenu closeSelf={() => setMobileMenuIsShown(false)} />
+      )}
+    </>
   );
 };
 
---
+++ frontend/src/utils/hooks.ts
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+
+export function useLockBodyScroll() {
+  useEffect((): (() => void) => {
+    // Get original body overflow
+    const originalStyle = window.getComputedStyle(document.body).overflow;
+
+    // Prevent scrolling on mount
+    document.body.style.overflow = 'hidden';
+
+    // Re-enable scrolling when component unmounts
+    return () => {
+      document.body.style.overflow = originalStyle;
+    };
+  }, []);
+}
