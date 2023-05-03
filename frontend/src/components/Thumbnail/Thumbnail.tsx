eed5d6709329c5e93c127a979941b3383ebe47c5
gregorsulcer <gregor.sulcer@student.um.si>
2023-05-03 11:39:33 +0200

    icons, thumbnail

--- frontend/src/components/Thumbnail/Thumbnail.tsx
+++ frontend/src/components/Thumbnail/Thumbnail.tsx
@@ -1,25 +1,24 @@
 import styles from './Thumbnail.module.css';
 import { FC } from 'react';
 import Image from 'next/image';
-import Test from '../../assets/gallery/test.jpeg';
 import { motion } from "framer-motion"
+import thumbnail from '../../assets/thumbnail.png';
 
 const Thumbnail: FC = () => {
   return (
     <div className={styles.Container}>
       <div className={styles.Info}>
-          <motion.div animate={{ x: 0 }} initial={{ x: -100, speed: 5}}>
-              <h1 className={styles.Title}>Mehanska obdelava kovin in druge storitve</h1>
-              <p className={styles.Description}>Kakovostne storitve za vaše inovativne ideje</p>
+          <motion.div animate={{ x: 0 }} initial={{ x: -100, speed: 5 }}>
+              <h1 className={`${styles.Title}`}>Mehanska obdelava kovin in druge storitve
+              </h1>
+              <p className={`${styles.Description}`}>Kakovostne storitve za vaše inovativne ideje</p>
           </motion.div>
       </div>
       <div className={styles.radialContainer}></div>
-      <Image
-        src={Test}
-        alt=""
-        layout="fill"
-        objectFit="cover"
-        className={styles.bgImage}
+      <Image src={thumbnail}
+             alt={'test'}
+             className={'mx-auto w-[75%] sm:mt-10'}
+             quality={100}
       />
     </div>
   );
